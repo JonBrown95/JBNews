9dfa3b14789a62a402f9131cdbf4a90ab9bcaf02
JonBrown95 <126886504+JonBrown95@users.noreply.github.com>
2023-05-09 15:54:50 +0100

    Completed Task 3.5
    Refactored to load the endpoints.json file :)

--- controllers.controllers.js
+++ controllers.controllers.js
@@ -1,4 +1,9 @@
 const { getTopics } = require("./models.models");
+const express = require("express");
+const fs = require("fs");
+const app = express();
+app.use(express.json());
+
 
 exports.getAllTopics = (req, res, next) => {
   getTopics()
@@ -11,37 +16,11 @@ exports.getAllTopics = (req, res, next) => {
 };
 
 exports.getAllEndpoints = (req, res, next) => {
-  const endpoints = {
-    "GET /api": {
-      description:
-        "serves up a json representation of all the available endpoints of the api",
-    },
-    "GET /api/topics": {
-      description: "serves an array of all topics",
-      queries: [],
-      exampleResponse: {
-        topics: [{ slug: "football", description: "Footie!" }],
-      },
-    },
-    "GET /api/articles": {
-      description: "serves an array of all topics",
-      queries: ["author", "topic", "sort_by", "order"],
-      exampleResponse: {
-        articles: [
-          {
-            title: "Seafood substitutions are increasing",
-            topic: "cooking",
-            author: "weegembump",
-            body: "Text from the article..",
-            created_at: "2018-05-30T15:59:13.341Z",
-            votes: 0,
-            comment_count: 6,
-            // Add more endpoints here as I go along
-          },
-        ],
-      },
-    },
-  };
-  res.status(200).send(endpoints);
+  fs.readFile("endpoints.json", (err, data) => {
+    if (err) {
+      return next(err);
+    }
+    const endpoints = JSON.parse(data.toString());
+    res.status(200).send(endpoints);
+  });
 };
-// Need to add more endpoints here once I add them
