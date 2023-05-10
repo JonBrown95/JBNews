239bca825876137c9d6e581da9dc1c4269c6f034
JonBrown95 <126886504+JonBrown95@users.noreply.github.com>
2023-05-10 11:48:06 +0100

    Completed task 4

--- models.models.js
+++ models.models.js
@@ -3,3 +3,18 @@ const db = require("./db/connection");
 exports.getTopics = () => {
   return db.query("SELECT * FROM topics;").then((result) => result.rows);
 };
+
+exports.getArticle = (articleId) => {
+  return db
+    .query("SELECT * FROM articles WHERE article_id = $1", [articleId])
+    .then((result) => {
+      if (result.rows.length === 0) {
+        return Promise.reject({
+          status: 400,
+          msg: "ID not found",
+        });
+      }
+      const article = result.rows[0];
+      return article;
+    });
+};
