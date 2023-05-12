d54821cbcf9a2453f3119ce2695ec0c273bcc130
JonBrown95 <126886504+JonBrown95@users.noreply.github.com>
2023-05-12 12:09:55 +0100

    Got website hosted :)

--- db/connection.js
+++ db/connection.js
@@ -1,6 +1,10 @@
 const { Pool } = require('pg');
 const ENV = process.env.NODE_ENV || 'development';
-const config =
+
+require('dotenv').config({
+  path: `${__dirname}/../.env.${ENV}`,
+});
+const config = 
   ENV === 'production'
     ? {
         connectionString: process.env.DATABASE_URL,
@@ -8,11 +12,9 @@ const config =
       }
     : {};
 
+console.log(process.env.DATABASE_URL)
 
 
-require('dotenv').config({
-  path: `${__dirname}/../.env.${ENV}`,
-});
 
 if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
   throw new Error('PGDATABASE or DATABASE_URL not set');
